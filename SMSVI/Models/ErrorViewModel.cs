916c3414819a79bd57a336bb058235f0db0aedc7
villanoti <89419902+villanoti@users.noreply.github.com>
2021-08-23 14:47:49 -0600

    Add files via upload

---
+++ SMSVI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SMSVI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
